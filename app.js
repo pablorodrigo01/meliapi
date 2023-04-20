910aaf1d7a7faa2d15716b57221e4fa6fe878a68
Pablo Rodrido <pablorodrigo210104@hotmail.com>
2023-04-20 13:00:50 -0300

    document

--- app.js
+++ app.js
@@ -65,6 +65,27 @@ app.get('/home', validateToken, async (req, res) => {
   }
 });
 
+app.get('/posts', validateToken, async (req, res) => { // Documentação do Mercado Livre
+  try {
+    const meliObject = new MeliObject(res.locals.access_token);
+    const user = await meliObject.get('/users/me');
+    const items = (await meliObject.get(`/users/${user.id}/items/search`)).results || [];
+    if (items.length) {
+      const result = [];
+      const promises = items.map(item_id => meliObject.get(`/items/${item_id}`));
+      for await (item of promises) {
+        result.push(item);
+      }
+      res.render('posts', { items: result });
+    } else {
+      res.status(404).send('no items were found :(');
+    }
+  } catch(err) {
+    console.log('Something went wrong', err);
+    res.status(500).send(`Error! ${err}`);
+  }
+});
+
 app.post('/post', validateToken, upload.single('picture'), async (req, res) => {
   try {
     const meliObject = new MeliObject(res.locals.access_token);
